be9e9eab9e3f1494ad41ad405c94f9cf2b9ca97f
Adithya S K <adithyaskolavi@gmail.com>
2024-03-02 23:06:03 +0530

    dpo finetuning scripts added - Adithya S K

---
+++ dpo_finetuning/dpo.sh
@@ -0,0 +1,19 @@
+accelerate launch dpo.py \
+    --model_name_or_path="Cognitive-Lab/Ambari-7B-Instruct-v0.1" \
+    --tokenizer_name_or_path="Cognitive-Lab/Ambari-7B-Instruct-v0.1" \
+    --num_train_epochs=1 \
+    --logging_steps=10 \
+    --save_steps=10 \
+    --per_device_train_batch_size=4 \
+    --per_device_eval_batch_size=1 \
+    --gradient_accumulation_steps=2 \
+    --gradient_checkpointing=True \
+    --lora_alpha=128 \
+    --lora_dropout=0.05 \
+    --lora_r=256 \
+    --lr_scheduler_type="cosine" \
+    --save_steps=1000 \
+    --warmup_steps=100 \
+    --weight_decay=0.05 \
+    --report_to="wandb" \
+    --output_dir="dpo"
