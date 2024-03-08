0c88a0cfc43fc9b8abdd63b62e4ddf4ac4429ac8
Adithya S K <adithyaskolavi@gmail.com>
2024-03-08 12:14:49 +0530

    added dataset download and corpus scripts - Adithya S K

---
+++ Indic_llm/dataset.py
@@ -0,0 +1,56 @@
+import logging
+import os
+import json
+
+import pandas as pd
+from datasets import load_dataset
+from tqdm import tqdm
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+
+def download(
+    dataset_name,
+    dataset_subset,
+    dataset_split="train",
+    ) -> None:
+    
+    dataset = load_dataset(
+        dataset_name,
+        dataset_subset,
+        split=dataset_split,
+        keep_in_memory=True
+    )
+    
+    return dataset
+
+def convert_to_corpus(
+    dataset_name,
+    dataset_subset,
+    dataset_split,
+    text_col,
+    output_file_name,
+    output_dir = "./corpus"
+    ) -> None:
+    
+    try:
+        dataset = load_dataset(dataset_name,dataset_subset,split=dataset_split)
+        train_df = pd.DataFrame(dataset)
+
+        os.makedirs(output_dir, exist_ok=True)
+        corpus_path = os.path.join(output_dir, output_file_name)
+
+        with open(corpus_path, "w") as file:
+            for index, value in tqdm(
+                train_df[text_col].items(), total=len(train_df)
+            ):
+                file.write(str(value) + "\n")
+
+    except Exception as e:
+        logger.error(f"Error creating the text corpus -> {e}")
+
+    return corpus_path
---
+++ scripts/download_dataset.py
@@ -0,0 +1,51 @@
+import argparse
+import logging
+import os
+
+import pandas as pd
+from datasets import load_dataset
+from tqdm import tqdm
+
+from Indic_llm.dataset import download
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger(__name__)
+
+class DownloadDataset():
+    def __init__(self) -> None:
+        pass
+    
+    def run(self):
+        parser = argparse.ArgumentParser(
+            description="Download Hugging Face dataset."
+        )
+        parser.add_argument(
+            "--hf-dataset",
+            required=True,
+            help="Name of the Hugging Face dataset (e.g., 'imdb').",
+        )
+        parser.add_argument(
+            "--hf-subset",
+            required=False,
+            help="Name of the path to language inside the dataset"
+        )
+        parser.add_argument(
+            "--dataset-split",
+            default="train",
+            help="Dataset split to use (default: 'train').",
+        )
+        
+
+        args = parser.parse_args()
+        download(
+            args.hf_dataset,
+            args.hf_subset,
+            args.dataset_split
+        )
+        
+if __name__ == "__main__":
+    download_dataset = DownloadDataset()
+    download_dataset.run()
